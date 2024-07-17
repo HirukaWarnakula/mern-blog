dc54ac2a597d7597b52b287272a231d4d3ed1218
21812576 <it21812576@my.sliit.lk>
2024-07-17 12:44:31 +0530

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcrypt';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res,next) => {
    
@@ -27,5 +28,40 @@ export const signup = async (req, res,next) => {
 
  
 };
+
+export const signin = async (req, res,next) => {
+
+   const { email, password } = req.body;
+   
+   if(!email || !password || email === '' || password === ''){
+    next(errorHandler(400,'All fields are required'));
+   }
+   
+   try{
+      const ValidUser = await User.findOne({email});
+      if(!ValidUser){
+         return next(errorHandler(404,'IUser not found'));
+      }
+      const ValidPassword =bcryptjs.compareSync(password,ValidUser.password); 
+      if(!ValidPassword){
+          return next(errorHandler(400,'Invalid password'));
+      }
+
+      const token = jwt.sign(
+         {id:ValidUser._id,},process.env.JWT_SECRET);
+   
+         const {password: pass, ...rest} =ValidUser._doc;
+
+         res.status(200).cookie('access_token',token,{
+            httpOnly:true,
+         }).json(rest);
+
+}catch(error){
+
+   next(error);
+
+ }
+
+}
 
  
